b801cced15f7ccc59b25b3461bee00fe37d7069a
avitiuk <avitiuk@barracuda.com>
2019-02-06 12:52:35 +0200

    grunt added

---
+++ gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      jshint: {all: ['*.js']},
+    });
+  
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+  };
